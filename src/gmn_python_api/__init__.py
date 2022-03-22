9fdfc7c574de5904595d1612ecb921bc4b95b8a3
Ricky Bassom <rickybas12@gmail.com>
2022-03-22 22:56:50 +0000

    Import all from top-level module user simplification

--- src/gmn_python_api/__init__.py
+++ src/gmn_python_api/__init__.py
@@ -1,1 +1,5 @@
 """GMN Python API."""
+from gmn_python_api.data_directory import *  # noqa: F403
+from gmn_python_api.trajectory_summary_reader import *  # noqa: F403
+
+__all__ = ["data_directory", "trajectory_summary_reader"]  # noqa: F405
