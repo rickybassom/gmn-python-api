10af1b6666234387828a5fb3e0d02c28ab2324bc
Ricky Bassom <rickybas12@gmail.com>
2022-04-17 00:58:33 +0100

    Include data_directory in __init__

--- src/gmn_python_api/__init__.py
+++ src/gmn_python_api/__init__.py
@@ -1,10 +1,12 @@
 """GMN Python API."""
+from gmn_python_api.data_directory import *  # noqa: F403
 from gmn_python_api.gmn_data_store_rest_api import *  # noqa: F403
 from gmn_python_api.iau_showers import *  # noqa: F403
 from gmn_python_api.meteor_summary_reader import *  # noqa: F403
 from gmn_python_api.meteor_summary_schema import *  # noqa: F403
 
 __all__ = [  # noqa: F405
+    "data_directory",
     "meteor_summary_reader",
     "iau_showers",
     "meteor_summary_schema",
